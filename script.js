77ca04b0e09269485fca5b0c3756a0a4dad46157
ektachitriw <152530727+ektachitriw@users.noreply.github.com>
2024-04-19 21:08:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const quizData = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "London", "Berlin", "Madrid"],
+      answer: "Paris"
+      question: "How much marks i will get in this project out of 20?",
+      options: ["20", "21", "69", "88"],
+      answer: "21"
+    },
+    {
+      question: "What is 2 + 2?",
+      options: ["3", "4", "5", "6"],
+      answer: "4"
+      question: "Who penned the book 'Wing of Fire'?",
+      options: ["APJ Abdul Kalam", "Narpender", "RaviKant", "Anchita Ma'am"],
+      answer: "APJ Abdul Kalam"
+    },
+    {
+      question: "Which is the largest planet in our solar system?",
+@@ -102,4 +102,4 @@ const quizData = [
+    clearInterval(timerInterval);
+  }
+
+  loadQuestion();
+  loadQuestion();
